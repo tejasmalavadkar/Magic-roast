4043614b4a395387b2d7259916519da6d6681882
Tejas Satish Malavadkar <tmalavadkar51617@gmail.com.com>
2026-01-27 15:00:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -13,29 +13,36 @@ import AboutPage from './pages/AboutPage';
 import ShopPage from './pages/ShopPage';
 import BlogsPage from './pages/BlogsPage';
 import ContactPage from './pages/ContactPage';
+import QualityPolicyPage from './pages/QualityPolicyPage'; // Import the Quality Policy page
+
+// HomePage component to render the main page content
+function HomePage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <ProductBasket />
+      <AboutCompany />
+      <VideosSection />
+      <BlogsSection />
+      <Footer />
+      <WhatsAppButton />
+    </main>
+  );
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={
-            <main className="min-h-screen">
-              <Header />
-              <HeroSection />
-              <AboutSection />
-              <ProductBasket />
-              <AboutCompany />
-              <VideosSection />
-              <BlogsSection />
-              <Footer />
-              <WhatsAppButton />
-            </main>
-          } />
+          <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/shop" element={<ShopPage />} />
           <Route path="/blogs" element={<BlogsPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/quality-policy" element={<QualityPolicyPage />} />
         </Routes>
       </div>
     </Router>
